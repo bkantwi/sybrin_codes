168feaa38d2432de7a5991e9419632ec45dc8e0d
bkantwi <bernardantwi2010@gmail.com>
2022-10-19 20:55:01 +0000

    queries complete

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sybrin_codes
 # reverse_words.py is a program that accepts user input and reverses the words and not the letters as an output
+
+#object_class_implementation.php implements and objects and classes using PHP
+
+#database_questions.php contains a few SQL queries
--- database_questions.php
+++ database_questions.php
@@ -3,8 +3,25 @@
 SELECT * FROM Employee ORDER BY Name ASC;
 
 <!-- Question 9 e -->
+SELECT b.Name,b.Surname,b.Position, 
+m.Name as ManagerName, 
+m.Position as ManagerPosition 
+FROM Employee 
+AS b INNER JOIN EmployeeManagers AS l 
+ON b.id = l.EmpID 
+LEFT JOIN Employee as m on 
+m.id = l.MID;
 
 
 <!-- Question 9 f -->
+SELECT b.Name,b.Surname,b.Position,m.Name as ManagerName, m.Position as ManagerPosition 
+FROM Employee 
+AS b LEFT JOIN EmployeeManagers AS l 
+ON b.id = l.EmpID 
+LEFT JOIN Employee as m on 
+m.id = l.MID;
+
 
 <!-- Question 9 g -->
+<!-- Assuming the table in question 9 e is called emp_managers -->
+SELECT ManagerName, COUNT(*) FROM emp_managers GROUP BY Name;
